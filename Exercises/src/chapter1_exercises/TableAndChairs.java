6e2f81211dc9d59c25a5da300e1b4da33029f574
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-27 09:30:58 -0500

    updated text

--- Exercises/src/chapter1_exercises/TableAndChairs.java
+++ Exercises/src/chapter1_exercises/TableAndChairs.java
@@ -4,7 +4,19 @@ public class TableAndChairs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
+		
+		
+		System.out.println("x                      x");
+		System.out.println("x                      x");
+		System.out.println("x                      x");
+		System.out.println("x                      x");
+		System.out.println("x                      x");
+		System.out.println("x      xxxxxxxxxx      x");
+		System.out.println("xxxxxx x        x xxxxxx");
+		System.out.println("x    x x        x x    x");
+		System.out.println("x    x x        x x    x");
+		System.out.println("x    x x        x x    x");
 	}
 
 }

3ab4b2eb834699af94959b947d180a809814946c
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-27 09:49:35 -0500

    updated text

--- Exercises/src/chapter1_exercises/FavoriteSong.java
+++ Exercises/src/chapter1_exercises/FavoriteSong.java
@@ -5,7 +5,11 @@ public class FavoriteSong {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.prinln(""Sweetner"");
+		System.out.println("Sweetner by Ariana Grande");
+		System.out.println("When life deals us cards");
+		System.out.println("Makes everything taste like it is salt");
+		System.out.println("Then you come through like the sweetner you are");
+		System.out.println("To bring the bitter paste to a halt");
 		
 	}
 

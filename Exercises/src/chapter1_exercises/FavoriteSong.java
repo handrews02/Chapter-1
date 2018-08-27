4e4e236709decf8c856f260964d24b90341f413d
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-27 09:47:44 -0500

    updated text

---
+++ Exercises/src/chapter1_exercises/FavoriteSong.java
@@ -0,0 +1,12 @@
+package chapter1_exercises;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.prinln(""Sweetner"");
+		
+	}
+
+}

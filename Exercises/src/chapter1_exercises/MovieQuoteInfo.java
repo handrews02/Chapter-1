4721df39653de2d92fc793c91a8b708f495f7793
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-27 09:39:39 -0500

    updated text

---
+++ Exercises/src/chapter1_exercises/MovieQuoteInfo.java
@@ -0,0 +1,14 @@
+package chapter1_exercises;
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I've never really given much thought to how I would die. But dying in the place of someone I love seems like a pretty good way to go");
+		System.out.println("Twilight");
+		System.out.println("Bella Swan");
+		System.out.println("2008");
+	}
+
+}

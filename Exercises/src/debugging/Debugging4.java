fffc7f03fcd2031218e6a65b57987424b87d4f48
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-28 08:56:30 -0500

    updated text

---
+++ Exercises/src/debugging/Debugging4.java
@@ -0,0 +1,13 @@
+package debugging;
+
+import javax.swing.JOptionPane;
+
+public class Debugging4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null, "First GUI program");
+	}
+
+}

00394c1133d956fdf84c4131fce7fc157ac9fa6e
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-28 09:04:16 -0500

    updated text

---
+++ Exercises/src/chapter1_exercises/SammysMotto2.java
@@ -0,0 +1,14 @@
+package chapter1_exercises;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("ssssssssssssssssssssssssssssssssssssss");
+ System.out.println("s                                    s");
+ System.out.println("s  Sammy's makes it fun in the sun.  s");
+ System.out.println("s                                    s");
+ System.out.println("ssssssssssssssssssssssssssssssssssssss");
+	}
+
+}

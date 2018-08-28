f057424e9814da2336b43ee9e1dce62d56d978cb
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-28 09:14:14 -0500

    updated text

--- Exercises/src/chapter1_exercises/SammysMotto2.java
+++ Exercises/src/chapter1_exercises/SammysMotto2.java
@@ -4,11 +4,11 @@ public class SammysMotto2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("ssssssssssssssssssssssssssssssssssssss");
- System.out.println("s                                    s");
- System.out.println("s  Sammy's makes it fun in the sun.  s");
- System.out.println("s                                    s");
- System.out.println("ssssssssssssssssssssssssssssssssssssss");
+ System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
+ System.out.println("s                                    S");
+ System.out.println("S  Sammy's makes it fun in the sun.  s");
+ System.out.println("s                                    S");
+ System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
 	}
 
 }

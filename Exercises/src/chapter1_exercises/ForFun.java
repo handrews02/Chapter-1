fb5df6df3f141b8138e852850c35c3d12d653926
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-29 09:41:38 -0500

    updated text

---
+++ Exercises/src/chapter1_exercises/ForFun.java
@@ -0,0 +1,10 @@
+package chapter1_exercises;
+
+public class ForFun {
+ static int myAge= 16;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Your age is " + myAge);
+	}
+
+}

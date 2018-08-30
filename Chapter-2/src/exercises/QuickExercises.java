432458ffaf0b93ebc9e8646927617967859e8fe6
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-30 09:54:13 -0500

    updated text

--- Chapter-2/src/exercises/QuickExercises.java
+++ Chapter-2/src/exercises/QuickExercises.java
@@ -1,10 +1,24 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class QuickExercises {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			String name;
+			int num1; 
+			int num2;
+			Scanner inputDevice = new Scanner(System.in);
+			
+			System.out.println("Please enter your name >> ");
+			name= inputDevice.next();
+			System.out.println("Please enter a number >>");
+			num1= 
 
+
+
+		
 	}
 
 }

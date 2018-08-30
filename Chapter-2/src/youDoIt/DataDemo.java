cf49efe19ad65e303adc2f00b65dfdf64b87a26f
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-30 09:15:41 -0500

    updated text

--- Chapter-2/src/youDoIt/DataDemo.java
+++ Chapter-2/src/youDoIt/DataDemo.java
@@ -8,7 +8,7 @@ double anInt=1234;
 byte aByte=12;
 short aShort=12;
 long aLong=1234567890987654321L;
-Double anotherInt= anInt * 10000000;
+double anotherInt= (anInt * 10000000);
 System.out.println("The int is " + anInt);
 System.out.println("The byte is " + aByte);
 System.out.println("The short is " + aShort);

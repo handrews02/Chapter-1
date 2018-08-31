cfd32cf1674ffb334bb480b6d0c15fb477abceb4
Hannah Andrews <handrews02@users.noreply.github.com>
2018-08-31 09:48:15 -0500

    updated text

--- Chapter-2/src/youDoIt/ArithmeticDemo3.java
+++ Chapter-2/src/youDoIt/ArithmeticDemo3.java
@@ -2,7 +2,7 @@ package youDoIt;
 
 import java.util.Scanner;
 
-public class ArithmeticDemo {
+public class ArithmeticDemo3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,7 +10,7 @@ int firstNumber;
 int secondNumber;
 int sum;
 int difference;
-int average;
+double average;
 
 Scanner input = new Scanner(System.in);
 
@@ -21,7 +21,7 @@ secondNumber = input.nextInt();
 
 sum = firstNumber + secondNumber;
 difference = firstNumber - secondNumber;
-average = sum / 2;
+average = (double) sum / 2;
 
 System.out.println(firstNumber + " + " + secondNumber + " is " + sum);
 System.out.println(firstNumber + " - " + secondNumber + " is " + difference);

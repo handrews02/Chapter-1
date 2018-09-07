dd77064336fd722321ace58120267f7f21dd3773
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-07 09:12:17 -0500

    updated text

---
+++ Chapter-2/src/youDoIt/Initals.java
@@ -0,0 +1,25 @@
+package youDoIt;
+
+import javax.swing.JOptionPane;
+
+public class Initals {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String H;
+		String M;
+		String A; 
+		
+		H= "H";
+		M= "M";
+		A= "A";
+		
+		
+		JOptionPane.showMessageDialog( null,  "My initals are " + H +" "+ M +" "+  A );
+		
+	}
+
+}

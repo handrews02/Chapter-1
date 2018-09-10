bfb9ccf394e32deb1eaf998123825c9a33272b98
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-10 09:15:04 -0500

    updated text

---
+++ Chapter-2/src/debugging/Debugging4.java
@@ -0,0 +1,10 @@
+package debugging;
+
+import javax.swing.JOptionPane;
+
+public class Debugging4 {
+	 public static void main(String[] args)
+	  {
+	     JOptionPane.showMessageDialog(null, "First GUI program");
+	  }
+}
